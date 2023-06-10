6a50ef9f0236d5140803493bca22392829cffed1
t.chelebi <t.chelebi@viseven.com>
2023-06-10 20:18:12 +0300

    feat(#2): update yml files and add Sentry init in _app.jsx file

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,20 @@
 import '../styles/globals.css';
 import React, { useEffect } from 'react';
+import * as Sentry from '@sentry/react';
 import i18n from '../i18n';
-import reportWebVitals from '../src/reportWebVitals';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN_KEY,
+  integrations: [
+    new Sentry.BrowserTracing({
+      tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
@@ -12,7 +25,5 @@ function MyApp({ Component, pageProps }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
-
-reportWebVitals();
 
 export default MyApp;
