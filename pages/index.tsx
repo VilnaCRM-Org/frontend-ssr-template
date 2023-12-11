669078eddd5e8daf935b74feb544cc1441c866f2
Dmytro Doroshko <dmitry.doroshko.99@gmail.com>
2023-12-11 17:08:56 +0200

    fix: fix eslint error

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,20 @@ import { useTranslation } from 'react-i18next';
 export default function Home() {
   const { t } = useTranslation();
 
+  const onClick = () => {
+    setTimeout(() => {
+      // eslint-disable-next-line no-console
+      console.log('clicked');
+    }, 2000);
+  };
+
   return (
       <div>
         <Head>
           <title>Frontend SSR template</title>
           <meta name="description" content="Frontend SSR template is used for bootstrapping a project."/>
         </Head>
-        <button onClick={() => {
-          setTimeout(() => {
-            console.log('clicked');
-          }, 2000);
-        }}>{t('click')}</button>
+        <button type='button' onClick={onClick}>{t('click')}</button>
         <h1>Frontend SSR template</h1>
       </div>
   );
