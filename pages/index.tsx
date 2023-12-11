7ef66792f841dad7c0094a5c635caa5729a7e6e5
Dmytro Doroshko <dmitry.doroshko.99@gmail.com>
2023-12-11 17:02:53 +0200

    fix: fix memory leak check

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import Head from 'next/head';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 export default function Home() {
+  const { t } = useTranslation();
+
   return (
       <div>
         <Head>
           <title>Frontend SSR template</title>
           <meta name="description" content="Frontend SSR template is used for bootstrapping a project."/>
         </Head>
+        <button onClick={() => {
+          setTimeout(() => {
+            console.log('clicked');
+          }, 2000);
+        }}>{t('click')}</button>
         <h1>Frontend SSR template</h1>
       </div>
   );
