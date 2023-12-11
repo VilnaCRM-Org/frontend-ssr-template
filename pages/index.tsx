3ed90e3a3a62f4ee488a3abc4f57098139b962f1
Dmytro Doroshko <dmitry.doroshko.99@gmail.com>
2023-12-11 16:52:40 +0200

    fix: fix lighthouse performance check

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,14 @@
 import Head from 'next/head';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 
 export default function Home() {
-  const { t} = useTranslation();
-
-  const click = () => {
-    setTimeout(() => {
-    }, 2000);
-  };
-
   return (
       <div>
         <Head>
-          <html lang="en"/>
           <title>Frontend SSR template</title>
           <meta name="description" content="Frontend SSR template is used for bootstrapping a project."/>
         </Head>
         <h1>Frontend SSR template</h1>
-        <button type='button' onClick={click}>{t('hello')}</button>
       </div>
   );
 }
