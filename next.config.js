f5ade43ceb0beb014a6f68ef2dfa1d6df284ea4f
DianaDuk <dianadukelska@gmail.com>
2025-01-08 22:47:12 +0200

    fix: Resolve test errors after updating Next.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,32 @@
+const TerserPlugin = require('terser-webpack-plugin')
 const LocalizationGenerator = require('./scripts/localizationGenerator');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-  swcMinify: true,
+  /*swcMinify: true,*/
   images: {
     unoptimized: true,
+  },
+  experimental: {
+    serverComponents: true,  
   },
   webpack: (config) => {
     const localizationGenerator = new LocalizationGenerator();
     localizationGenerator.generateLocalizationFile();
 
+    if (!isServer) {
+      config.optimization.minimizer.push(
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true, 
+            },
+          },
+        })
+      );
+    }
     return config;
   },
 };
